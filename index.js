62e5454fed08d230c5325c512e5191002791ce47
Shumona-Akter <shumona.cse.bd@gmail.com>
2023-05-02 16:40:15 +0600

    cors install and cors setup

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var cors = require('cors')
 
+app.use(cors())
 const chefData = require('./data/chefData.json')
 app.get('/', (req, res) => {
   res.send(chefData)
