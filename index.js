48b2fa1f93f69b98b1c422bb4a8bc0f7e2267360
Shumona-Akter <shumona.cse.bd@gmail.com>
2023-05-02 23:31:15 +0600

    details data generate

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const app = express()
 const port = 3000
 var cors = require('cors')
 
+const detailsData = require("./data/chefDetails.json")
+
 app.use(cors())
 const chefData = require('./data/chefData.json')
 app.get('/', (req, res) => {
   res.send(chefData)
 })
 
+app.get('/:id', (req, res) =>{
+  const id = parseInt(req.params.id)
+  const details = detailsData.find(n=> parseInt(n._id) === id)
+  res.send(details)
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
